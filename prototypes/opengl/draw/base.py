66e17f5c86233486cfde1962a76196e805a8a0f9
servrcube <63092977+servrcube@users.noreply.github.com>
2021-10-11 10:23:41 +0200

    modengl examples

---
+++ prototypes/opengl/draw/base.py
@@ -0,0 +1,18 @@
+import os
+
+import moderngl_window as mglw
+
+
+class Example(mglw.WindowConfig):
+    gl_version = (3, 3)
+    title = "ModernGL Example"
+    window_size = (1280, 720)
+    aspect_ratio = 16 / 9
+    resizable = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @classmethod
+    def run(cls):
+        mglw.run_window_config(cls)
